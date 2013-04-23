eb4eea31b3c4126f920341cd909a2d5c834abddf
Josh Purvis <joshua.purvis@gmail.com>
2013-04-23 15:33:12 -0400

    added grid lines

--- js/app.js
+++ js/app.js
@@ -40,89 +40,12 @@ var weather = (function (parent, $) {
         });
     }
 
-    self.draw = function(lat, lon) {
-
-        /* draw the location marker on map */
-        var coordProjection = self.projection([lon, lat]);
-        self.marker = self.group.append('circle')
-            .attr('cx', coordProjection[0])
-            .attr('cy', coordProjection[1])
-            .attr('r', 5)
-            .attr('class', 'symbol');
-
-        /* create weather chart */
-        self.drawWeather(lat, lon);
-
-    }
-
-    self.drawWeather = function(lat, lon) {
-
-        self.weatherSvg = d3.select('#svgWeather')
-            .attr("width", self.weatherWidth + self.weatherMargin.left + self.weatherMargin.right)
-            .attr("height", self.weatherHeight + self.weatherMargin.top + self.weatherMargin.bottom);
-
-        self.weatherSvg.selectAll('g').remove();
-
-        self.weatherChart = self.weatherSvg
-            .append("g")
-                .attr("transform", "translate(" + self.weatherMargin.left + "," + self.weatherMargin.top + ")");
-
-        self.weatherChart.append("text")
-                .attr("x", (self.weatherWidth / 2) + 30)
-                .attr("y", 0 - (self.weatherMargin.top / 2) + 30)
-                .attr("text-anchor", "middle")
-                .attr("class", "chart-title")
-                .text("48 Hour Forecast");
-
-        d3.json(self.apiPrefix + lat + ',' + lon, function(e, response) {
-            var data = response.hourly.data;
-
-            data.forEach(function(d) {
-                d.date = d.time * 1000;
-            });
-
-            self.weatherX.domain(d3.extent(data, function(d) { return d.date; }));
-            self.weatherY.domain(d3.extent(data, function(d) { return d.temperature; }));
-
-            self.weatherChart.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + self.weatherHeight + ")")
-                .call(self.weatherAxisX);
-
-            self.weatherChart.append("g")
-                .attr("class", "y axis")
-                .call(self.weatherAxisY)
-                .append("text")
-                    .attr("transform", "rotate(-90)")
-                    .attr("y", 20)
-                    .style("text-anchor", "end")
-                    .text("Temperature (F)");
-
-            var path = self.weatherChart.append("path")
-                .datum(data)
-                .attr("class", "line")
-                .attr("d", self.weatherLine)
-                .attr("fill", "none");
-
-            /* animate the line */
-            var totalPathLength = path.node().getTotalLength();
-            path
-                .attr("stroke-dasharray", totalPathLength + " " + totalPathLength)
-                .attr("stroke-dashoffset", totalPathLength)
-                .transition()
-                    .duration(1000)
-                    .ease("linear")
-                    .attr("stroke-dashoffset", 0);
-
-        });
-    }
-
     self.ready = function(error, us) {
-        /* map */
+        /* map variables */
         self.mapWidth = 480
         self.mapHeight = 250;
 
-        /* weather chart */
+        /* weather chart variables */
         self.weatherMargin = {top: 20, right:20, bottom: 30, left: 50};
         self.weatherWidth = 960 - self.weatherMargin.left - self.weatherMargin.right;
         self.weatherHeight = 500 - self.weatherMargin.top - self.weatherMargin.bottom;
@@ -173,19 +96,123 @@ var weather = (function (parent, $) {
         self.weatherY = d3.scale.linear()
             .range([self.weatherHeight, 0]);
 
-        self.weatherAxisX = d3.svg.axis()
-            .scale(self.weatherX)
-            .orient("bottom");
+        self.weatherAxisX = function() {
+            return d3.svg.axis()
+                .scale(self.weatherX)
+                .orient("bottom")
+                .ticks(10);
+        }
 
-        self.weatherAxisY = d3.svg.axis()
+        self.weatherAxisY = function() {
+            return d3.svg.axis()
             .scale(self.weatherY)
-            .orient("left");
+            .orient("left")
+            .ticks(10);
+        }
 
         self.weatherLine = d3.svg.line()
             .x(function(d) { return self.weatherX(d.date); })
             .y(function(d) { return self.weatherY(d.temperature); })
             .interpolate('linear');
 
+    }
+
+    self.draw = function(lat, lon) {
+
+        /* draw the location marker on map */
+        var coordProjection = self.projection([lon, lat]);
+        self.marker = self.group.append('circle')
+            .attr('cx', coordProjection[0])
+            .attr('cy', coordProjection[1])
+            .attr('r', 5)
+            .attr('class', 'symbol');
+
+        /* create weather chart */
+        self.drawWeather(lat, lon);
+
+    }
+
+    self.drawWeather = function(lat, lon) {
+
+        self.weatherSvg = d3.select('#svgWeather')
+            .attr("width", self.weatherWidth + self.weatherMargin.left + self.weatherMargin.right)
+            .attr("height", self.weatherHeight + self.weatherMargin.top + self.weatherMargin.bottom);
+
+        self.weatherSvg.selectAll('g').remove();
+
+        self.weatherChart = self.weatherSvg
+            .append("g")
+                .attr("transform", "translate(" + self.weatherMargin.left + "," + self.weatherMargin.top + ")");
+
+        self.weatherChart.append("text")
+                .attr("x", (self.weatherWidth / 2) + 30)
+                .attr("y", 0 - (self.weatherMargin.top / 2) + 30)
+                .attr("text-anchor", "middle")
+                .attr("class", "chart-title")
+                .text("48 Hour Forecast");
+
+        d3.json(self.apiPrefix + lat + ',' + lon, function(e, response) {
+            var data = response.hourly.data;
+
+            data.forEach(function(d) {
+                d.date = d.time * 1000;
+            });
+
+            self.weatherX.domain(d3.extent(data, function(d) { return d.date; }));
+            self.weatherY.domain(d3.extent(data, function(d) { return d.temperature; }));
+
+            /* add tick lines */
+            self.weatherChart.append("g")
+                    .attr("class", "grid")
+                    .attr("transform", "translate(0," + self.weatherHeight + ")")
+                    .call(self.weatherAxisX()
+                        .tickSize(-self.weatherHeight, 0, 0)
+                        .tickFormat("")
+                    );
+
+            self.weatherChart.append("g")
+                .attr("class", "grid")
+                .call(self.weatherAxisY()
+                    .tickSize(-self.weatherWidth, 0, 0)
+                    .tickFormat("")
+                );
+
+            /* add axes */
+            self.weatherChart.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + self.weatherHeight + ")")
+                .call(self.weatherAxisX()
+                );
+
+            self.weatherChart.append("g")
+                .attr("class", "y axis")
+                .call(
+                    self.weatherAxisY()
+                )
+                .append("text")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 20)
+                    .style("text-anchor", "end")
+                    .text("Temperature (F)");
+
+            /* draw line */
+            var path = self.weatherChart.append("path")
+                .datum(data)
+                .attr("class", "line")
+                .attr("d", self.weatherLine)
+                .attr("fill", "none");
+
+            /* animate the line */
+            var totalPathLength = path.node().getTotalLength();
+            path
+                .attr("stroke-dasharray", totalPathLength + " " + totalPathLength)
+                .attr("stroke-dashoffset", totalPathLength)
+                .transition()
+                    .duration(1000)
+                    .ease("linear")
+                    .attr("stroke-dashoffset", 0);
+
+        });
     }
 
     self.search = function() {
